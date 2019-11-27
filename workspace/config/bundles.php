156bd07bd86475ceda0768bbf268a211dd2e9b3c
Zi-long QIU <zilong.qiu@gmail.com>
2019-11-27 17:03:54 +0100

    [master] feat: require Symfony API bundles

--- workspace/config/bundles.php
+++ workspace/config/bundles.php
@@ -2,4 +2,7 @@
 
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
+    FOS\RestBundle\FOSRestBundle::class => ['all' => true],
+    JMS\SerializerBundle\JMSSerializerBundle::class => ['all' => true],
+    Nelmio\ApiDocBundle\NelmioApiDocBundle::class => ['all' => true],
 ];
