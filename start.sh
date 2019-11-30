4f529c27607cdffedd62d1990e3ce4d4d3c63d4e
Zi-long QIU <zilong.qiu@gmail.com>
2019-11-30 02:02:08 +0000

    [master] feat: improve start.sh with a success message

--- start.sh
+++ start.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
+# Start services
 docker-compose up -d --force-recreate
+
+# Initialize project
 docker-compose exec phpfpm composer self-update --no-interaction
 docker-compose exec phpfpm composer install --no-interaction
+# TODO enable it just after the test
+#docker-compose exec phpfpm composer dump-env prod
 docker-compose exec phpfpm ./bin/console doctrine:migrations:migrate --allow-no-migration --no-interaction
 docker-compose exec phpfpm rm -rf var/cache/
 docker-compose exec phpfpm ./bin/console cache:warmup
+
+echo 'Congratulations! The API is accessible at http://localhost:8080/.'
