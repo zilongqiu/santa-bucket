f33413aafd512439090d4a8d1c5ae557e72f5835
Zi-long QIU <zilong.qiu@gmail.com>
2019-12-06 23:36:44 +0000

    [master] feat: configure to prod environment

--- start.sh
+++ start.sh
@@ -5,13 +5,12 @@ sudo chown -R www-data:www-data $(pwd)
 sudo chmod -R 777 $(pwd)
 
 # Start services
-docker-compose up -d --force-recreate -V
+docker-compose up -d --force-recreate
 
 # Initialize project
 docker-compose exec phpfpm composer self-update --no-interaction
 docker-compose exec phpfpm composer install --no-interaction --no-dev --optimize-autoloader
-# TODO enable it just after the test
-#docker-compose exec phpfpm composer dump-env prod
+docker-compose exec phpfpm composer dump-env prod
 docker-compose exec phpfpm ./bin/console doctrine:migrations:migrate --allow-no-migration --no-interaction
 docker-compose exec phpfpm ./bin/console cache:warmup
 docker-compose exec phpfpm rm -rf var/cache/
