c72af1b85209251b69d0540aa070f3b61bfe1f24
Zi-long QIU <zilong.qiu@gmail.com>
2019-12-06 23:05:39 +0000

    [master] refactor: remove profiler component

--- start.sh
+++ start.sh
@@ -5,11 +5,11 @@ docker-compose up -d --force-recreate -V
 
 # Initialize project
 docker-compose exec phpfpm composer self-update --no-interaction
-docker-compose exec phpfpm composer install --no-interaction
+docker-compose exec phpfpm composer install --no-interaction --no-dev --optimize-autoloader
 # TODO enable it just after the test
 #docker-compose exec phpfpm composer dump-env prod
 docker-compose exec phpfpm ./bin/console doctrine:migrations:migrate --allow-no-migration --no-interaction
-docker-compose exec phpfpm rm -rf var/cache/
 docker-compose exec phpfpm ./bin/console cache:warmup
+docker-compose exec phpfpm rm -rf var/cache/
 
 echo 'Congratulations! The API is accessible at http://localhost:8080/.'
